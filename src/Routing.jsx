a3ca0954076b22fdb943469a2d4cbda589324fd9
attotem <vshinkarenko21@gmail.com>
2024-02-03 18:43:35 +0100

    Initial commit

---
+++ src/Routing.jsx
@@ -0,0 +1,30 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import Login from "./Components/Login/login";
+import DefaultLayout from "./Components/SideBar/DefaultLayout";
+import Category from "./Components/AI_category/category"
+import SignUp from "./Components/SingUp/singup"
+import Chart from "./Components/DashBoard/linechart"
+import Dashboard from "./Components/DashBoard/Dashboard";
+
+const Routing = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate replace to="/login" />} />
+      <Route path="/login" element={<Login />} />
+
+      <Route path="/category" element={<DefaultLayout><Category /></DefaultLayout>} />
+
+      <Route path="/another-route" element={<DefaultLayout><div>ne 123</div></DefaultLayout>} />
+
+      <Route path="/singup" element={<SignUp />} />
+
+      <Route path="/dashboard" element={<DefaultLayout><Dashboard /></DefaultLayout>} />
+
+      <Route path="*" element={<DefaultLayout><div>No such directory</div></DefaultLayout>} />
+    </Routes>
+  );
+};
+
+
+
+export default Routing;
---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+// import reportWebVitals from './reportWebVitals';
+import Routing from './Routing';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+
+  <React.StrictMode>
+    
+    <Router>
+      <Routing />
+    </Router>
+
+  </React.StrictMode>
+);
+
+
+// reportWebVitals();
